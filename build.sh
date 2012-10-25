197f6d00ba73e9e67199fb583bb4e495086d849c
Jiri Moskovcak <moskovcak@gmail.com>
2012-10-25 19:48:59 +0200

    minor fixes to the build script
    - create required bin directories
    - silence the mkdir output if the dir exists

--- build.sh
+++ build.sh
@@ -5,14 +5,15 @@ git submodule update
 # make jTrac
 pushd `pwd`
 cd jars/jTrac/
-mkdir bin 2>&1 > /dev/null
+mkdir bin &> /dev/null
 make jar
 popd
 # make jBugzilla
 pushd `pwd`
 cd jars/jBugzilla
-mkdir bin 2>&1 > /dev/null
+mkdir bin &> /dev/null
 make jar
 popd
 # make the bot
+mkdir bin &> /dev/null
 make
